fd036b4836d1ea0eb882a5e8d5732a4d54fd9ecb
baoluchuling <baoluchuling@126.com>
2022-11-24 20:28:23 +0800

    Update main.rs

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -7,11 +7,6 @@ pub use initialize::{listener::Listener, route::Route};
 pub use utils::string::StringExtension;
 
 fn main() {
-    let path = [
-        "/".to_string(),
-        "/plus".to_string(),
-        "/?version".to_string()
-    ].to_vec();
 
     let _route_map = StringExtension::split(fs::read_to_string("/Users/admin/Desktop/rust_study/webserver/src/assets/route.list").unwrap(), "\n");
 
